69089627b1371cb053f75e65286cb532c7cffc1e
Costopoulos <costopoulos.constantinos@gmail.com>
2023-06-25 11:24:57 +0300

    Add folder deletion

---
+++ AWSLambda/delete/emptyS3Service.js
@@ -0,0 +1,33 @@
+const AWS = require('aws-sdk');
+const s3 = new AWS.S3();
+
+async function emptyS3Directory(bucket, dir) {
+    const listParams = {
+        Bucket: bucket,
+        Prefix: dir
+    };
+
+    const listedObjects = await s3.listObjectsV2(listParams).promise();
+
+    if (listedObjects.Contents.length === 0) return;
+
+    const deleteParams = {
+        Bucket: bucket,
+        Delete: { Objects: [] }
+    };
+
+    listedObjects.Contents.forEach(({ Key }) => {
+        deleteParams.Delete.Objects.push({ Key });
+    });
+
+    await s3.deleteObjects(deleteParams).promise();
+
+    if (listedObjects.IsTruncated) await emptyS3Directory(bucket, dir);
+    
+    return {
+        statusCode: 200,
+        body: JSON.stringify('Successfully deleted'),
+    };
+}
+
+module.exports.emptyS3Directory = emptyS3Directory;
