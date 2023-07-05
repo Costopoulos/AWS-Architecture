4387f95f2e33e85d0da8d6e366e19aa7ec1ef0f2
Costopoulos <costopoulos.constantinos@gmail.com>
2023-07-05 10:11:42 +0300

    Finish modify's lambda code

--- AWSLambda/modify/index.js
+++ AWSLambda/modify/index.js
@@ -9,20 +9,28 @@ exports.handler = async (event) => {
     let FILE_TO_MODIFY = event.pathParameters.file;
     FILE_TO_MODIFY = `${SUBFOLDER}/${FOLDER_TO_MODIFY_FROM}/${FILE_TO_MODIFY}`;
     
-    const fileName = event.body.split('\r\n')[1].split(';')[2].split('=')[1].replace(/^"|"$/g, '').trim();
+    console.log("event ", event);
+    const fileName = event.body.split('\r\n')[1].split(';')[1].split('=')[1].replace(/^"|"$/g, '').trim();
+    let binary64Part = '';
     let fileContent = '';
     if (fileName.endsWith(".txt")) {
         fileContent = event.body.split('\r\n')[4].trim();
     }
+    else {
+        binary64Part = event.body.split('\r\n')[3];
+        console.log('binary64Part: ', binary64Part);
+        fileContent = Buffer.from(binary64Part, 'base64');
+    }
+    console.log('name: ', fileName)
+    console.log('content: ', fileContent)
+    // fileContent += `\n\nProcess Timestamp: ${new Date().toISOString()}`
     
-    // Set the parameters for the S3 API call
     const params = {
-      Bucket: BUCKET_NAME,
-      Key: FILE_TO_MODIFY,
-      Body: fileContent,
-    };
-    console.log(fileContent);
-    
+        Bucket: BUCKET_NAME,
+        Key: FILE_TO_MODIFY,
+        Body: fileContent,
+        ContentEncoding: 'base64'
+    }
     const response = await s3.putObject(params).promise();
     return response;
 };
